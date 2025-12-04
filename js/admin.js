07247e91264d231f98c7fcbab5cc03da0951e593
Hptktvn <phucthinhktvn@gmail.com>
2025-12-04 20:12:32 +0700

    up_lan_2

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,5 @@
+// Kiểm tra người dùng đã đăng nhập hay chưa
+// Nếu chưa đăng nhập thì chuyển hướng về login.html
 if (localStorage.getItem("logged_in") !== "true") {
   window.location.href = "login.html";
 }
